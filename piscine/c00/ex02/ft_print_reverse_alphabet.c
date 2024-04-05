7c2260ea3c7908d7e2553bb13377c53563d7e7b0
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2024-04-05 15:05:22 +0100

    Add void ft_print_reverse_alphabet(void);

---
+++ piscine/c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void ft_putchar(char c) {
+    write(1, &c, 1);
+}
+
+void ft_print_reverse_alphabet(void) {
+    int i = 122;
+    for(i; i>= 97; i --) {
+        ft_putchar(i);
+    }
+}
+
+int main() {
+    ft_print_reverse_alphabet();
+    ft_putchar('\n');
+    return 0;
+}
