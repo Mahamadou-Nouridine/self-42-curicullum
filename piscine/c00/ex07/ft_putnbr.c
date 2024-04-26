6fd18fb4a9ccb11db27a3d0295ac09d5fbe1420d
Mahamadou Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2024-04-26 21:37:00 +0100

    Update ft_putnbr.c

--- piscine/c00/ex07/ft_putnbr.c
+++ piscine/c00/ex07/ft_putnbr.c
@@ -24,16 +24,3 @@ void ft_putnbr(int nb)
     }
 }
 
-// int main(void) {
-//     ft_putnbr(0);
-//     ft_putchar('\n');
-//     ft_putnbr(-2147483648);
-//     ft_putchar('\n');
-//     ft_putnbr(2147483647);
-//     ft_putchar('\n');
-//     ft_putnbr(42);
-//     ft_putchar('\n');
-//     ft_putnbr(-42);
-//     ft_putchar('\n');
-//     return 0;
-// }
